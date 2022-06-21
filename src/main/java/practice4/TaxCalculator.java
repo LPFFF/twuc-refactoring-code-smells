7a1086bc3898ad35c476fa89d2faa313ca090e4c
lvpengfei <pengfei.lv@thoughtworks.com>
2022-06-21 21:56:52 +0800

    extraction of constants

--- src/main/java/practice4/TaxCalculator.java
+++ src/main/java/practice4/TaxCalculator.java
@@ -1,7 +1,15 @@
 package practice4;
 
 public class TaxCalculator {
+
+    private static final double ZERO_POINT_ZERO_EIGHT = 0.08;
+    private static final double ZERO_POINT_ZERO_TWO = 0.02;
+    private static final double ZERO_POINT_ZERO_ZERO_TWO = 0.004;
+    private static final double THREE_FOUR_FIVE = 3.45;
+    private static final double EIGHT_SEVEN_POINT_THREE_SEVEN = 87.37;
+    private static final int NINETY_NINE = 99;
+
     public double calculateTax(double salary) {
-        return salary * (0.08 + 0.02 + 0.004 + 3.45) - 87.37 * 0.08 / 99;
+        return salary * (ZERO_POINT_ZERO_EIGHT + ZERO_POINT_ZERO_TWO + ZERO_POINT_ZERO_ZERO_TWO + THREE_FOUR_FIVE) - EIGHT_SEVEN_POINT_THREE_SEVEN * ZERO_POINT_ZERO_EIGHT / NINETY_NINE;
     }
 }
